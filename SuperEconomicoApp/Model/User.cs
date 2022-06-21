e6e48506369bcdb7a37baffb36e35c29d32899cf
estebancerrato <estebancerrato@hotmail.com>
2022-06-20 22:16:48 -0600

    modifique model user

--- SuperEconomicoApp/Model/User.cs
+++ SuperEconomicoApp/Model/User.cs
@@ -6,8 +6,23 @@ namespace SuperEconomicoApp.Model
 {
     public class User
     {
-        public string Username { get; set; }
+        public int id { get; set; }
 
-        public string Password { get; set; }
+        public string name { get; set; }
+
+        public string lastname { get; set; }
+
+        public string birthdate { get; set; }
+
+        public string email { get; set; }
+
+        public string password { get; set; }
+
+        public byte[] image { get; set; }
+
+        public string state { get; set; }
+
+        public string cod_temp { get; set; }
+
     }
 }
