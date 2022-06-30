a70ab33d72943a6a066112e19fbda6d0356db284
BrayanUTH <10brayanalvarez@gmail.com>
2022-06-30 08:02:22 -0600

    Agregando correctamente los parametros del POST order

--- SuperEconomicoApp/Services/OrderService.cs
+++ SuperEconomicoApp/Services/OrderService.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
@@ -32,18 +33,35 @@ namespace SuperEconomicoApp.Services
             try
             {
                 var coordinates = Settings.Coordinates.Split(',');
-                //string parametersGet = "?latitude=" + coordinates[0] + "&longitude=" + coordinates[1];
-                //Uri requestUri = new Uri(ApiMethods.URL_ORDERS + parametersGet);
-                Uri requestUri = new Uri(ApiMethods.URL_ORDERS+"/");
+                ////string parametersGet = "?latitude=" + coordinates[0] + "&longitude=" + coordinates[1];
+                ////Uri requestUri = new Uri(ApiMethods.URL_ORDERS + parametersGet);
+                //Uri requestUri = new Uri(ApiMethods.URL_ORDERS + "/");
+                //var client = new HttpClient();
+                //var json = JsonConvert.SerializeObject(order);
+                //var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
+                //var result = await client.PostAsync(requestUri, contentJson);
+                //if (result.StatusCode == HttpStatusCode.OK)
+                //{
+                //    RemoveItemsFromCart();
+                //    return true;
+                //}
+
                 var client = new HttpClient();
+                Uri requestUri = new Uri(ApiMethods.URL_ORDERS);
+                client.DefaultRequestHeaders.Add("latitude", coordinates[0]);
+                client.DefaultRequestHeaders.Add("longitude", coordinates[1]);
+
                 var json = JsonConvert.SerializeObject(order);
-                var contentJson = new StringContent(json, Encoding.UTF8, "application/json");
-                var result = await client.PostAsync(requestUri, contentJson);
-                if (result.StatusCode == HttpStatusCode.OK)
+                HttpContent content = new StringContent(json);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                var response = await client.PostAsync(requestUri, content);
+
+                if (response.StatusCode == HttpStatusCode.OK)
                 {
                     RemoveItemsFromCart();
                     return true;
                 }
+
             }
             catch (Exception ex)
             {
